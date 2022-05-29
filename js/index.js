30a47a0bc9236b63b4fad6132c57f161680e1997
JyrexRonaldo <jyrexgt38@gmail.com>
2022-05-29 10:41:27 +0000

    Add form element for collecting data

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 const tableBody = document.querySelector("tbody");
-
+const newBook = document.querySelector(".library button");
+const bookForm = document.querySelector(".form");
+const formButton = document.querySelector(".submit button");
+const bookTitle = document.querySelector("input[name='title']");
+const bookAuthor = document.querySelector("input[name='author']");
+const bookPages = document.querySelector("input[name='pages']");
+const bookRead = document.querySelector("input[name='read']");
 let myLibrary = [];
+
+addBookToLibrary("Titan", "Eren", 90, "read");
+addBookToLibrary("Titan", "Eren", 90, "read");
+addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+
+newBook.addEventListener("click", () => {
+    bookForm.style.display = "flex";
+});
+
+formButton.addEventListener("click", function() {
+    tableBody.innerHTML = "";
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+    bookForm.style.display = "none";
+    displayBook();
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,12 +33,10 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
+  console.log("Hello")
 }
 
-addBookToLibrary("Titan", "Eren", 90, "read");
-addBookToLibrary("Titan", "Eren", 90, "read");
-addBookToLibrary("Titan", "Eren", 90, "read");
-addBookToLibrary("Titan", "Eren", 90, "read");
+
 
 function displayBook() {
   for (let i = 0; i < myLibrary.length; i++) {
@@ -32,4 +51,4 @@ function displayBook() {
 
 displayBook();
 
-console.log(tableBody);
+console.log(bookRead.value);
