54707b84513fc96e7e61abbaf295a701b6578d0f
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-10-05 12:09:39 +0000

    Implement html structure and dialog box

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-const bookLibrary = document.querySelector("tbody")
+const dialogBox = document.querySelector("dialog");
+const newBookBtn = document.querySelector(".new-book");
+const bookLibrary = document.querySelector("tbody");
+const formButton = document.querySelector("form button")
 
 
 const myLibrary = [new Book("The Hobbit", "J.R.R Tolkien", 295, true),
+new Book("The Hobbit", "J.R.R Tolkien", 295, false),
 new Book("The Hobbit", "J.R.R Tolkien", 295, true),
-new Book("The Hobbit", "J.R.R Tolkien", 295, true)]; 
+new Book("The Hobbit", "J.R.R Tolkien", 295, false)
+]; 
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -21,16 +26,32 @@ function displayBook() {
         const title = document.createElement("td");
         const author = document.createElement("td");
         const pages = document.createElement("td");
+        const readBtn = document.createElement("button");
         const readStatus = document.createElement("td");
+        const deletebtn = document.createElement("button");
+        const deleteCell = document.createElement("td");
+        const bookItem = document.createElement("tr");
+        
         title.append(book.title);
         author.append(book.author);
         pages.append(book.pages);
-        readStatus.append(book.readStatus);
-        const bookItem = document.createElement("tr");
-        bookItem.append(title, author, pages, readStatus);
+        readStatus.append(readBtn);
+        readBtn.append(book.readStatus);
+        deleteCell.append(deletebtn)
+        bookItem.append(title, author, pages, readStatus, deleteCell);
         bookLibrary.append(bookItem)
+        
+        deletebtn.textContent = "Delete";
+        readBtn.textContent = book.read ? "read" : "not read";
     });
 }
 
-
 displayBook();
+
+newBookBtn.addEventListener("click", () => {
+    dialogBox.showModal()
+});
+
+formButton.addEventListener("click", (e) => {
+    console.log("yeah");
+})
